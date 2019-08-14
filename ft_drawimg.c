be9f48dd0d470f16e00be504b5c3f7a3168508c8
Drackonis <kergastel@hotmail.fr>
2019-08-14 15:59:51 +0200

    drawer

--- ft_drawimg.c
+++ ft_drawimg.c
@@ -6,7 +6,7 @@
 /*   By: rkergast <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/14 14:22:26 by rkergast          #+#    #+#             */
-/*   Updated: 2019/08/14 15:16:23 by rkergast         ###   ########.fr       */
+/*   Updated: 2019/08/14 15:59:30 by rkergast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void		ft_putpix(t_data *data, int x, int y)
 {
-	data->img.data[x * 4 +
-		y * 4 + 2] = data->color.r;
-	/*data->img.data[(x * (data->img.bpp / 8)) +
-		(y * data->img.size_line)] = data->color.r;
+	/*data->img.data[x * 4 +
+		y * 4 + 2] = data->color.r;*/
+	data->img.data[(x * 4) +
+		(y * data->winheight * 4)] = data->color.r;
+	data->img.data[(x * 4) +
+		(y * data->winheight * 4) + 1] = data->color.g;
 	data->img.data[(x * (data->img.bpp / 8)) +
-		(y * data->img.size_line) + 1] = data->color.g;
-	data->img.data[(x * (data->img.bpp / 8)) +
-		(y * data->img.size_line) + 2] = data->color.b;*/
+		(y * data->winheight * 4) + 2] = data->color.b;
 }
 
 void		ft_drawimg(t_data *data)
@@ -29,16 +29,19 @@ void		ft_drawimg(t_data *data)
 	int i = 0;
 	int j = 0;
 
-printf("DATA NB LINES--->%d\n", data->nblin);
+	printf("DATA NB LINES--->%d\n", data->nblin);
 	printf("DATA NB COLUM--->%d\n", data->nbcol);
-		printf("WINSIZEWIDTH = %d\n", data->winwidth);
-			printf("WINSIZEHEIGHT = %d\n", data->winheight);
-				printf("SPACE = %d\n\n", data->space);
+	printf("WINSIZEWIDTH = %d\n", data->winwidth);
+	printf("WINSIZEHEIGHT = %d\n", data->winheight);
+	printf("SPACE = %d\n\n", data->space);
 
-	while (i++ < 1100)
+	while (i < data->winheight * data->winwidth)
 	{
-		j = 0;
-		//while (j++ < 52/*data->img.size_line*/)
+		i++;
+		//while (j < data->winwidth)
+		//{
 			ft_putpix(data, i, j);
+		//	j++;
+		//}
 	}
 }
